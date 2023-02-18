8f79aeaad54131fd682fa2ea8065ea0439cd4be2
Brian Wu <brian.wu@stanford.edu>
2023-02-18 02:32:51 -0800

    bugfixes

--- Scripts/data_collection_utils.py
+++ Scripts/data_collection_utils.py
@@ -4,14 +4,16 @@ from threading import Thread
 from video_test_flight import flight
 import cv2
 import time
+import os
 
 # Helper Functions for Data Collectin
 def take_individual_picture(drone):
     drone.streamon()
     frame_read = drone.get_frame_read()
+    image_directory = os.path.join(os.path.abspath(os.path.join('..')), "Images")
     title = "image_" + str(time.time()) + ".jpg"
     current_frame = frame_read.frame
-    cv2.imwrite(title, current_frame)
+    cv2.imwrite(os.path.join(image_directory, title), current_frame)
     
     
 def stream_video():
@@ -23,7 +25,8 @@ def stream_video():
     
     # Static Vars
     keepRecording = True
-    title = "test_video.avi"
+    video_directory = os.path.join(os.path.abspath(os.path.join('..')), "Videos")
+    title = os.path.join(video_directory, "test_video.mp4")
     fps = 30
     
     # Record Video
@@ -31,7 +34,7 @@ def stream_video():
         height, width, _ = frame_read.frame.shape
         video = cv2.VideoWrite(
             title,
-            cv2.VideoWriter_fourcc(*"XVID"), # Video Codec, can use "mp4v" or "X264" as alternatives
+            cv2.VideoWriter_fourcc(*"mp4v"), # Video Codec, can use "mp4v" or "X264" as alternatives
             30, # Frame Rate
             (width, height) # Video Resolution
         )
@@ -46,7 +49,7 @@ def stream_video():
     recorder.start()
     
     # Run this function to get the actions for the flight
-    flight()
+    flight(drone)
         
     keepRecording = False
     recorder.join()
